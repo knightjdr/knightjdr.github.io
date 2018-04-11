b7aa49c661c4959340acd7d47a3f370244db2c88
James Knight <knightjdr@gmail.com>
2018-04-10 21:45:15 -0400

    Create landing page

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,26 +1,31 @@
 import React from 'react';
 
+import ComputerBack from '../assets/images/computer-back-min.svg';
+import Description from '../assets/images/description-min.svg';
+import Name from '../assets/images/name-min.svg';
+
 import './landing.css';
 
 const Landing = () => (
-  <div>
-    <div className="Landing">
-      <div className="text-light">
-        Light
-      </div>
-      <div className="text-dark">
-        Light
-      </div>
-      <div className="accent">
-        Light
-      </div>
+  <div className="Landing">
+    <div className="Landing-name">
+      <img
+        alt="James Knight, PhD"
+        src={Name}
+      />
     </div>
-    <div className="navbar" />
-    <div className="text-light" />
-    <div className="section-1" />
-    <div className="section-2" />
-    <div className="section-3" />
-    <div className="section-4" />
+    <div className="Landing-image">
+      <img
+        alt="James at computer"
+        src={ComputerBack}
+      />
+    </div>
+    <div className="Landing-description">
+      <img
+        alt="Programmer and scientist"
+        src={Description}
+      />
+    </div>
   </div>
 );
 export default Landing;
