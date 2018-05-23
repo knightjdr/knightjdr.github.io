c95fac809bceaf18116f8e8cdb9fa8c4ab156c4b
James Knight <knightjdr@gmail.com>
2018-05-23 11:47:16 -0400

    Add 404 page

--- src/404/404.js
+++ src/404/404.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Computer404 from './assets/computer404-min.svg';
+
+import './404.css';
 
 const Missing = () => (
-  <div />
+  <div className="Missing">
+    <img
+      alt="404"
+      className="Missing-image"
+      src={Computer404}
+    />
+    <div className="Missing-text">
+      Unfortunately the page you were looking for could not be found.
+    </div>
+    <NavLink to="/">
+      <button className="button-pointer Missing-home-button">
+        Home
+      </button>
+    </NavLink>
+  </div>
 );
 export default Missing;
